d81503738a9397f0065159097b607b88089e301f
Sam Breed <sam@quickleft.com>
2014-08-14 22:10:28 -0600

    renaming build/ -> dist/

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var mocha = require('gulp-mocha-phantomjs');
 
 gulp.task('test', function() {
   return gulp.src('./test/index.html')
-    .pipe(mocha());
+    .pipe(mocha({
+      reporter: 'dot'
+    }));
 });
 
 gulp.task('default', ['test']);
