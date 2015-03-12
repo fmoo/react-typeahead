fad1ad2a318b3271f845bfa3338e38dfdd88118f
Anis Elleuch <vadmeste@gmail.com>
2015-03-12 10:16:30 +0100

    Use querySelector to select the input node when the user provides a ReactClass instead of the standard typeahead html input

--- src/typeahead/index.js
+++ src/typeahead/index.js
@@ -20,6 +20,7 @@ var Typeahead = React.createClass({
       React.PropTypes.func,
       React.PropTypes.string
     ]),
+    inputQuerySel: React.PropTypes.string,
     maxVisible: React.PropTypes.number,
     options: React.PropTypes.array,
     allowCustomValues: React.PropTypes.number,
@@ -34,6 +35,7 @@ var Typeahead = React.createClass({
       options: [],
       customClasses: {},
       CustomComponent: "input",
+      inputQuerySel: "input",
       allowCustomValues: 0,
       defaultValue: "",
       placeholder: "",
@@ -67,7 +69,7 @@ var Typeahead = React.createClass({
   },
 
   setEntryText: function(value) {
-    this.refs.entry.getDOMNode().value = value;
+    this.refs.container.getDOMNode().querySelector(this.props.inputQuerySel).value = value;
     this._onTextEntryUpdated();
   },
 
@@ -109,7 +111,7 @@ var Typeahead = React.createClass({
   },
 
   _onOptionSelected: function(option, event) {
-    var nEntry = this.refs.entry.getDOMNode();
+    var nEntry = this.refs.container.getDOMNode().querySelector(this.props.inputQuerySel);
     nEntry.focus();
     nEntry.value = option;
     this.setState({visible: this.getOptionsForValue(option, this.props.options),
@@ -119,7 +121,7 @@ var Typeahead = React.createClass({
   },
 
   _onTextEntryUpdated: function() {
-    var value = this.refs.entry.getDOMNode().value;
+    var value = this.refs.container.getDOMNode().querySelector(this.props.inputQuerySel).value;
     this.setState({visible: this.getOptionsForValue(value, this.props.options),
                    selection: null,
                    entryValue: value});
@@ -190,7 +192,7 @@ var Typeahead = React.createClass({
     var classList = React.addons.classSet(classes);
 
     return (
-      <div className={classList}>
+      <div ref="container" className={classList}>
         <this.props.CustomComponent ref="entry" type="text"
           placeholder={this.props.placeholder}
           className={inputClassList}
