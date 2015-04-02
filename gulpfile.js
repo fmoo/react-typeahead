14142238952a080af783f7438c67b08db6dbae35
vegetableman <vigneshanand17@gmail.com>
2015-04-03 00:28:01 +0530

    Added gulp build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha-phantomjs');
+var react = require('gulp-react');
 
 gulp.task('test', function() {
   return gulp.src('./test/index.html')
     .pipe(mocha({
       reporter: 'dot'
     }));
+});
+
+gulp.task('build', function () {
+    return gulp.src('./src/**')
+        .pipe(react({
+            harmony: true
+        }))
+        .pipe(gulp.dest('./lib'));
 });
 
 gulp.task('default', ['test']);
