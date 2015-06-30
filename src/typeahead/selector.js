8905feb522743979a3d206bcdba108b1e42c7195
Loïc Padier <loic.padier@gmail.com>
2015-06-30 16:03:54 +0200

    Moved state that was in selector into index, moved getSelector and nav to index, and added _hasHint helper

--- src/typeahead/selector.js
+++ src/typeahead/selector.js
@@ -29,13 +29,6 @@ var TypeaheadSelector = React.createClass({
     };
   },
 
-  getInitialState: function() {
-    return {
-      selectionIndex: this.props.selectionIndex,
-      selection: this.getSelectionForIndex(this.props.selectionIndex)
-    };
-  },
-
   render: function() {
     var classes = {
       "typeahead-selector": true
@@ -43,7 +36,6 @@ var TypeaheadSelector = React.createClass({
     classes[this.props.customClasses.results] = this.props.customClasses.results;
     var classList = classNames(classes);
 
-    var results = [];
     // CustomValue should be added to top of results list with different class name
     var customValue = null;
     var customValueOffset = 0;
@@ -51,7 +43,7 @@ var TypeaheadSelector = React.createClass({
       customValueOffset++;
       customValue = (
         <TypeaheadOption ref={this.props.customValue} key={this.props.customValue}
-          hover={this.state.selectionIndex === 0}
+          hover={this.props.selectionIndex === 0}
           customClasses={this.props.customClasses}
           customValue={this.props.customValue}
           onClick={this._onClick.bind(this, this.props.customValue)}>
@@ -64,7 +56,7 @@ var TypeaheadSelector = React.createClass({
       var displayString = this.props.displayOption(result, i);
       return (
         <TypeaheadOption ref={displayString} key={displayString}
-          hover={this.state.selectionIndex === i + customValueOffset}
+          hover={this.props.selectionIndex === i + customValueOffset}
           customClasses={this.props.customClasses}
           onClick={this._onClick.bind(this, result)}>
           { displayString }
@@ -81,59 +73,8 @@ var TypeaheadSelector = React.createClass({
     );
   },
 
-  setSelectionIndex: function(index) {
-    this.setState({
-      selectionIndex: index,
-      selection: this.getSelectionForIndex(index),
-    });
-  },
-
-  getSelectionForIndex: function(index) {
-    if (index === null) {
-      return null;
-    }
-    if (index === 0 && this.props.customValue !== null) {
-      return this.props.customValue;
-    }
-
-    if (this.props.customValue !== null) {
-      index -= 1;
-    }
-
-    return this.props.options[index];
-  },
-
   _onClick: function(result, event) {
     return this.props.onOptionSelected(result, event);
-  },
-
-  _nav: function(delta) {
-    if (!this.props.options && this.props.customValue === null) {
-      return;
-    }
-    var newIndex = this.state.selectionIndex === null ? (delta == 1 ? 0 : delta) : this.state.selectionIndex + delta;
-    var length = this.props.options.length;
-    if (this.props.customValue !== null) {
-      length += 1;
-    }
-
-    if (newIndex < 0) {
-      newIndex += length;
-    } else if (newIndex >= length) {
-      newIndex -= length;
-    }
-
-    var newSelection = this.getSelectionForIndex(newIndex);
-    this.setState({selectionIndex: newIndex,
-                   selection: newSelection});
-  },
-
-  navDown: function() {
-    this._nav(1);
-  },
-
-  navUp: function() {
-    this._nav(-1);
   }
 
 });
