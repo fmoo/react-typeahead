827cc7465353e9acc740171e74726bd92042e833
Lewis Monteith <lewis.monteith@jobsite.co.uk>
2017-06-16 15:38:42 +0100

    Converted webpack config to spaces

--- webpack.config.js
+++ webpack.config.js
@@ -6,52 +6,52 @@ const wpDefinePlugin = require('webpack').DefinePlugin;
 const wpLoaderOptionsPlugin = require('webpack').LoaderOptionsPlugin;
 
 module.exports = (ARGS = {}) => {
-	const PATH_ROOT = pathJoin(__dirname);
-	const PATH_APP_ENTRY = pathJoin(PATH_ROOT, 'src');
-	const PATH_OUTPUT = pathJoin(PATH_ROOT, 'dist');
+  const PATH_ROOT = pathJoin(__dirname);
+  const PATH_APP_ENTRY = pathJoin(PATH_ROOT, 'src');
+  const PATH_OUTPUT = pathJoin(PATH_ROOT, 'dist');
   
-	return Object.assign({}, {
-		devtool: 'cheap-module-eval-source-map',
-		entry: {
-			app: [PATH_APP_ENTRY],
-		},
-		output: {
-			path: PATH_OUTPUT,
-			filename: 'react-typeahead.js',
-			libraryTarget: 'commonjs'
-		},
-		module: {
-			rules: [
-				{
-					test: /\.js$/,
-					include: [
-						pathJoin(PATH_APP_ENTRY),
-					],
-					exclude: [],
-					loader: 'babel-loader',
-					options: {
-						presets: ['react', 'es2015', 'stage-0'],
-						plugins: [],
-					},
-				},
-			],
-		},
-		plugins: [
-			new wpDefinePlugin({
-				'process.env': { 
-					NODE_ENV: JSON.stringify('production')
-				},
-			}),
-			new wpUglifyJsPlugin({
-				compress: {
-					warnings: false, 
-					unused: true, 
-					dead_code: true
-				},
-				output: {
-					comments: false
-				}
-			})
-		],
-	});
+  return Object.assign({}, {
+    devtool: 'cheap-module-eval-source-map',
+    entry: {
+      app: [PATH_APP_ENTRY],
+    },
+    output: {
+      path: PATH_OUTPUT,
+      filename: 'react-typeahead.js',
+      libraryTarget: 'commonjs'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: [
+            pathJoin(PATH_APP_ENTRY),
+          ],
+          exclude: [],
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015', 'stage-0'],
+            plugins: [],
+          },
+        },
+      ],
+    },
+    plugins: [
+      new wpDefinePlugin({
+        'process.env': { 
+          NODE_ENV: JSON.stringify('production')
+        },
+      }),
+      new wpUglifyJsPlugin({
+        compress: {
+          warnings: false, 
+          unused: true, 
+          dead_code: true
+        },
+        output: {
+          comments: false
+        }
+      })
+    ],
+  });
 };
