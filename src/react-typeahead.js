25d2321449546c47206b0404842adba7e43a0d98
Brodrick Childs <brodrick.childs@gmail.com>
2019-06-12 08:42:52 -0700

    Add typeahead selector

--- src/react-typeahead.js
+++ src/react-typeahead.js
@@ -1,7 +1,9 @@
 var Typeahead = require('./typeahead');
 var Tokenizer = require('./tokenizer');
+var TypeaheadSelector = require('./typeahead/selector')
 
 module.exports = {
   Typeahead: Typeahead,
-  Tokenizer: Tokenizer
+  Tokenizer: Tokenizer,
+  TypeaheadSelector: TypeaheadSelector
 };
